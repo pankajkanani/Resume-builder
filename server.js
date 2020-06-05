535047df7e2c6182213b8056834d1b2c3d306fbb
Ashutosh Kumar <kumar.ashutosh920@gmail.com>
2020-06-05 12:15:33 +0530

    server added for heroku

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const http = require('http');
+const server = http.createServer(app);
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+/***************************************************************************/
